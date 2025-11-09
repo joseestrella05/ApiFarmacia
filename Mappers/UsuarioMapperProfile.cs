4b5bef4c941009026fedca4ca1b274a2a6481d9e
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-09 13:57:15 -0400

    Add UsuarioMapperProfile for DTO mappings

---
+++ Mappers/UsuarioMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using ApiFarmacia.Dto;
+using ApiFarmacia.Models;
+using AutoMapper;
+
+namespace ApiFarmacia.Mappers;
+
+public class UsuarioMapperProfile : Profile
+{
+    public UsuarioMapperProfile()
+    {
+        CreateMap<Usuario, UsuarioReadDto>();
+        CreateMap<RegistroUsuarioDto, Usuario>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+    }
+}
