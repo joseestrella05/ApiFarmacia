8323fae83ec0d1c557565031b9509b25b52b1805
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-09 13:57:49 -0400

    Add user and authentication service interfaces

---
+++ Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using ApiFarmacia.Models;
+using System.Security.Claims;
+
+namespace ApiFarmacia.Services;
+
+public interface ITokenService
+{
+    string GenerarToken(Usuario usuario);
+    string GenerarRefreshToken();
+    ClaimsPrincipal? ValidarToken(string token);
+}
---
+++ Services/IUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using ApiFarmacia.Dto;
+
+namespace ApiFarmacia.Services;
+
+public interface IUsuarioService
+{
+    Task<(bool Exito, string Mensaje, AuthResponseDto? Data)> RegistrarAsync(RegistroUsuarioDto dto);
+    Task<(bool Exito, string Mensaje, AuthResponseDto? Data)> LoginAsync(LoginDto dto);
+    Task<(bool Exito, string Mensaje, UsuarioReadDto? Data)> ObtenerPerfilAsync(int usuarioId);
+    Task<(bool Exito, string Mensaje, UsuarioReadDto? Data)> ActualizarPerfilAsync(int usuarioId, ActualizarUsuarioDto dto);
+    Task<(bool Exito, string Mensaje)> CambiarPasswordAsync(int usuarioId, CambiarPasswordDto dto);
+    Task<(bool Exito, string Mensaje, AuthResponseDto? Data)> RefreshTokenAsync(string refreshToken);
+}
+
