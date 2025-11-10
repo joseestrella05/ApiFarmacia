7d78744822d802c1c85e9ccbba1e475ec0a601eb
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-10 10:49:49 -0400

    Add CambiarRoleDto

---
+++ Dto/CambiarRolDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFarmacia.Dto;
+
+public class CambiarRolDto
+{
+    [Required(ErrorMessage = "El nuevo rol es obligatorio")]
+    public string NuevoRol { get; set; } = string.Empty;
+}
+
